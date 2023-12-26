8f5105daad37745e0104cc47c31030c93f39b2c4
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2023-12-26 15:00:57 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -4,45 +4,55 @@
 
 namespace Transport {
 
-void ProcessRequests(std::ostream& out, Catalogue& catalogue) {
-    size_t requests_count;
-    std::cin >> requests_count;
-    for (size_t i = 0; i < requests_count; ++i) {
-        std::string keyword, line;
-        std::cin >> keyword;
-        std::getline(std::cin, line);
-        if (keyword == "Bus") {
-            std::string route_number = line.substr(1, line.npos);
-            if (catalogue.FindRoute(route_number)) {
-                out << "Bus " << route_number << ": " << catalogue.RouteInformation(route_number).stops_count << " stops on route, "
-                    << catalogue.RouteInformation(route_number).unique_stops_count << " unique stops, " << std::setprecision(6)
-                    << catalogue.RouteInformation(route_number).route_length << " route length\n";
+    void PrintBusData(std::ostream& out, const Catalogue& catalogue, const std::string& route_number) {
+        if (catalogue.FindRoute(route_number)) {
+            out << "Bus " << route_number << ": " << catalogue.RouteInformation(route_number).stops_count << " stops on route, "
+                << catalogue.RouteInformation(route_number).unique_stops_count << " unique stops, " << std::setprecision(6)
+                << catalogue.RouteInformation(route_number).route_length << " route length\n";
+        }
+        else {
+            out << "Bus " << route_number << ": not found\n";
+        }
+    }
+
+    void PrintStopData(std::ostream& out, const Catalogue& catalogue, const std::string& stop_name) {
+        if (catalogue.FindStop(stop_name)) {
+            out << "Stop " << stop_name << ": ";
+            std::unordered_set<std::string> buses = catalogue.GetBusesOnStop(stop_name);
+            if (!buses.empty()) {
+                out << "buses ";
+                for (const auto& bus : buses) {
+                    out << bus << " ";
+                }
+                out << "\n";
             }
             else {
-                out << "Bus " << route_number << ": not found\n";
+                out << "no buses\n";
             }
         }
-        if (keyword == "Stop") {
-            std::string stop_name = line.substr(1, line.npos);
-            if (catalogue.FindStop(stop_name)) {
-                out << "Stop " << stop_name << ": ";
-                std::set<std::string> buses = catalogue.GetBusesOnStop(stop_name);
-                if (!buses.empty()) {
-                    out << "buses ";
-                    for (const auto& bus : buses) {
-                        out << bus << " ";
-                    }
-                    out << "\n";
-                }
-                else {
-                    out << "no buses\n";
-                }
+        else {
+            out << "Stop " << stop_name << ": not found\n";
+        }
+    }
+
+    void ProcessRequests(std::ostream& out, std::istream& in, Catalogue& catalogue) {
+        size_t requests_count;
+        in >> requests_count;
+        in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        for (size_t i = 0; i < requests_count; ++i) {
+            std::string keyword, line;
+            in >> keyword;
+            std::getline(in, line);
+
+            if (keyword == "Bus") {
+                std::string route_number = line.substr(1, line.npos);
+                PrintBusData(out, catalogue, route_number);
             }
-            else {
-                out << "Stop " << stop_name << ": not found\n";
+            else if (keyword == "Stop") {
+                std::string stop_name = line.substr(1, line.npos);
+                PrintStopData(out, catalogue, stop_name);
             }
         }
     }
-}
-    
 }
