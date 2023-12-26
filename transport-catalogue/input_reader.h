39a53b12f9b0c13ad804616698c4339fd494ffa0
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2023-12-26 13:18:33 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,12 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <iostream>
+
+namespace Transport {
+    
+void FillCatalogue(std::istream& in, Catalogue& catalogue);
+std::pair<std::vector<std::string>, bool> FillRoute(std::string& line);
+    
+}
