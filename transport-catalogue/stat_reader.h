07403a7d2f152c63451353cb27892edd42f7c71d
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2023-12-26 17:21:38 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -5,6 +5,8 @@
 #include <iostream>
 namespace Transport {
     
-void ProcessRequests(std::ostream& out, Catalogue& catalogue);
+void ProcessRequests(std::istream& in, Catalogue& catalogue, std::ostream& out);
+void PrintBusData(std::string& line, Catalogue& catalogue, std::ostream& out);
+void PrintStopData(std::string& line, Catalogue& catalogue, std::ostream& out);  
     
 }
