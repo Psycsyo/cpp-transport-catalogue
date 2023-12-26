9f27ac3e13c6da3a22111b57d2b85fb3d81ac4a1
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2023-12-26 13:19:55 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,10 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <iostream>
+namespace Transport {
+    
+void ProcessRequests(std::ostream& out, Catalogue& catalogue);
+    
+}
