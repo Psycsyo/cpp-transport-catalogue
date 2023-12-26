5ea812978a211c1ea2b0909590ed2ba611e4b73b
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2023-12-26 13:19:00 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,10 @@
-// место для вашего кода
+#include "input_reader.h"
+#include "stat_reader.h"
+
+using namespace Transport;
+
+int main() {
+    Catalogue catalogue;
+    FillCatalogue(std::cin, catalogue);
+    ProcessRequests(std::cout, catalogue);
+}
