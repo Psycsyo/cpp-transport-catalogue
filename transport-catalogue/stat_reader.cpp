c990049bd858b375cbbdf540f08dd47586f8f1b1
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-01-22 09:18:39 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -2,58 +2,56 @@
 
 #include <iomanip>
 
-namespace Transport {
+namespace transport {
 
-void PrintBusData(std::ostream& out, const Catalogue& catalogue, const std::string& route_number) {
-        if (catalogue.FindRoute(route_number)) {
-            out << "Bus " << route_number << ": " << catalogue.RouteInformation(route_number).stops_count << " stops on route, "
-                << catalogue.RouteInformation(route_number).unique_stops_count << " unique stops, " << std::setprecision(6)
-                << catalogue.RouteInformation(route_number).route_length << " route length\n";
+void ProcessRequests(std::istream& in, Catalogue& catalogue, std::ostream& out) {
+    size_t requests_count;
+    in >> requests_count;
+    for (size_t i = 0; i < requests_count; ++i) {
+        std::string keyword, line;
+        in >> keyword;
+        std::getline(in, line);
+        if (keyword == "Bus") {
+            PrintRoute(line, catalogue, out);
+        }
+        if (keyword == "Stop") {
+            PrintStop(line, catalogue, out);
+        }
+    }
+}
+
+void PrintRoute(std::string& line, Catalogue& catalogue, std::ostream& out) {
+    std::string bus_number = line.substr(1, line.npos);
+    if (catalogue.FindRoute(bus_number)) {
+        out << "Bus " << bus_number << ": " << catalogue.GetBusStat(bus_number)->stops_count << " stops on route, "
+            << catalogue.GetBusStat(bus_number)->unique_stops_count << " unique stops, " << std::setprecision(6)
+            << catalogue.GetBusStat(bus_number)->route_length << " route length, "
+            << catalogue.GetBusStat(bus_number)->curvature << " curvature\n";
+    }
+    else {
+        out << "Bus " << bus_number << ": not found\n";
+    }
+}
+
+void PrintStop(std::string& line, Catalogue& catalogue, std::ostream& out) {
+    std::string stop_name = line.substr(1, line.npos);
+    if (catalogue.FindStop(stop_name)) {
+        out << "Stop " << stop_name << ": ";
+        auto buses = catalogue.GetBusesByStop(stop_name);
+        if (!buses.empty()) {
+            std::cout << "buses ";
+            for (const auto& bus : buses) {
+                out << bus << " ";
+            }
+            out << "\n";
         }
         else {
-            out << "Bus " << route_number << ": not found\n";
+            out << "no buses\n";
         }
     }
-
-    void PrintStopData(std::ostream& out, const Catalogue& catalogue, const std::string& stop_name) {
-        if (catalogue.FindStop(stop_name)) {
-            out << "Stop " << stop_name << ": ";
-            std::set<std::string> buses = catalogue.GetBusesOnStop(stop_name);
-            if (!buses.empty()) {
-                out << "buses ";
-                for (const auto& bus : buses) {
-                    out << bus << " ";
-                }
-                out << "\n";
-            }
-            else {
-                out << "no buses\n";
-            }
-        }
-        else {
-            out << "Stop " << stop_name << ": not found\n";
-        }
+    else {
+        out << "Stop " << stop_name << ": not found\n";
     }
+}
 
-    void ProcessRequests(std::istream& in, Catalogue& catalogue, std::ostream& out) {
-        size_t requests_count;
-        in >> requests_count;
-        in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        for (size_t i = 0; i < requests_count; ++i) {
-            std::string keyword, line;
-            in >> keyword;
-            std::getline(in, line);
-
-            if (keyword == "Bus") {
-                std::string route_number = line.substr(1, line.npos);
-                PrintBusData(out, catalogue, route_number);
-            }
-            else if (keyword == "Stop") {
-                std::string stop_name = line.substr(1, line.npos);
-                PrintStopData(out, catalogue, stop_name);
-            }
-        }
-    }
-    
 }
