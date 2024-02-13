8971bb522e09db9873bad98877a896444da6f3b5
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-02-13 13:18:39 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,16 +1,28 @@
 #include "json_reader.h"
 #include "request_handler.h"
 
+#include <fstream>
+
 int main() {
+    setlocale(LC_ALL, "rus");
+    std::fstream input("input.json");
+    if (!input) std::cout << "input error" << std::endl;
+
+    std::fstream output("output.xml");
+    if (!output) std::cout << "output error" << std::endl;
+
+    JsonReader json_doc(input);
     transport::Catalogue catalogue;
-    JsonReader json_doc(std::cin);
-    
     json_doc.FillCatalogue(catalogue);
-    
-    const auto& stat_requests = json_doc.GetStatRequests();
+
+    const auto& stat_requests = json_doc.GetDataRequests();
     const auto& render_settings = json_doc.GetRenderSettings().AsMap();
     const auto& renderer = json_doc.FillRenderSettings(render_settings);
 
-    RequestHandler rh(catalogue, renderer);
-    json_doc.ProcessRequests(stat_requests, rh);
+    RequestHandler request(catalogue, renderer);
+    json_doc.ProcessRequests(stat_requests, request);
+    request.RenderMap().Render(output);
+
+    input.close();
+    output.close();
 }
