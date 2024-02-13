1d75aaa3fa4e06159cfdc99ab145e4c021dac421
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-02-13 13:21:52 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -4,24 +4,20 @@
 #include "transport_catalogue.h"
 #include "map_renderer.h"
 
-#include <sstream>
-
 class RequestHandler {
 public:
-    RequestHandler(const transport::Catalogue& catalogue, const renderer::MapRenderer& renderer)
-        : catalogue_(catalogue)
-        , renderer_(renderer)
-    {
-    }
-
-    std::optional<transport::BusStat> GetBusStat(const std::string_view bus_number) const;
-    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
-    bool IsBusNumber(const std::string_view bus_number) const;
-    bool IsStopName(const std::string_view stop_name) const;
+    RequestHandler(const transport::Catalogue& catalogue, const renderer::MapRenderer& renderer) 
+        : catalogue_(catalogue), renderer_(renderer) {}
 
     svg::Document RenderMap() const;
 
+    bool HasBus(const std::string_view bus_number) const;
+    bool HasStop(const std::string_view stop_name) const;
+
+    std::optional<transport::data::Bus> GetBusData(const std::string bus_number) const;
+    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
 private:
     const transport::Catalogue& catalogue_;
     const renderer::MapRenderer& renderer_;
+
 };
