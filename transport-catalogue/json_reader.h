188168eff5c6389d9a490e8529a7b5afa0c12a05
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-02-13 13:18:17 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,36 +1,34 @@
 #pragma once
-
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
 #include "request_handler.h"
 
-#include <iostream>
+#include <sstream>
 
 class JsonReader {
 public:
-    JsonReader(std::istream& input)
-        : input_(json::Load(input))
-    {}
+    JsonReader(std::istream& input) : input_(json::Load(input)) {}
 
-    const json::Node& GetBaseRequests() const;
-    const json::Node& GetStatRequests() const;
+    void ProcessRequests(const json::Node& stat_requests, RequestHandler& request) const;
+
+    const json::Node& GetDataRequests() const;
     const json::Node& GetRenderSettings() const;
-
-    void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
 
     void FillCatalogue(transport::Catalogue& catalogue);
     renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
-
-    const json::Node PrintRoute(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node PrintStop(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node PrintMap(const json::Dict& request_map, RequestHandler& rh) const;
-
 private:
     json::Document input_;
-    json::Node dummy_ = nullptr;
+    json::Node dummy_;
 
-    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& request_map) const;
-    void FillStopDistances(transport::Catalogue& catalogue) const;
-    std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillRoute(const json::Dict& request_map, transport::Catalogue& catalogue) const;
+    const json::Node& GetBaseRequests() const;
+
+    json_reader::Stop FillStop(const json::Dict& request_map) const;
+    json_reader::Bus FillBus(const json::Dict& request_map) const;
+
+    const json::Node PrintBus(const json::Dict& request_map, RequestHandler& request) const;
+    const json::Node PrintStop(const json::Dict& request_map, RequestHandler& request) const;
+    const json::Node PrintMap(const json::Dict& request_map, RequestHandler& request) const;
+
+    svg::Color GetColor(const json::Node& color_element) const;
 };
