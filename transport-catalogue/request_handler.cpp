8b63263418fa85c28b39107e4fbc98d0ee985035
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-18 11:49:40 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,21 +1,55 @@
 #include "request_handler.h"
 
-svg::Document RequestHandler::RenderMap() const {
-    return renderer_.GetSVG(catalogue_.GetSortedAllBuses());
-}
+namespace tc_project::request_handler {
 
-std::optional<transport::data::Bus> RequestHandler::GetBusData(const std::string bus_number) const {
-    return catalogue_.GetBusData(bus_number);
-}
+    RequestHandler::RequestHandler(const transport_catalogue::TransportCatalogue& db, const map_renderer::MapRenderer& renderer)
+            : db_(db), renderer_(renderer) {
+    }
 
-const std::set<std::string> RequestHandler::GetBusesByStop(std::string_view stop_name) const {
-    return catalogue_.FindStopsForBus(stop_name);
-}
+    std::optional<BusInfo> RequestHandler::GetBusInfo(const std::string_view& name) const {
+        BusInfo bus_info{};
+        auto bus = db_.FindBus(std::string(name));
+        if(!bus) {
+            return std::nullopt;
+        }
+        auto index_routes = db_.GetIndexRoutes();
+        int stops_count = static_cast<int>(index_routes.at(name)->stops.size());
+        bus_info.stops_count = stops_count;
+        auto stops = index_routes.at(name)->stops;
+        bus_info.unique_stops_count = static_cast<int>(std::unordered_set<const Stop*>(stops.begin(), stops.end()).size());
+        bus_info.distance = 0;
+        double geo_dist = 0.0;
+        for (int i = 0; i < stops_count - 1; ++i) {
+            const Stop* cur_stop = stops[i];
+            const Stop* next_stop = stops[i + 1];
+            geo_dist += geo::ComputeDistance({cur_stop->coordinates.lat, cur_stop->coordinates.lng},
+                                             {next_stop->coordinates.lat, next_stop->coordinates.lng});
+            bus_info.distance += db_.GetDistance(cur_stop, next_stop);
+        }
+        bus_info.curvature = bus_info.distance/geo_dist;
+        return bus_info;
+    }
 
-bool RequestHandler::HasBus(const std::string_view bus_number) const {
-    return catalogue_.FindBus(bus_number);
-}
+    std::optional<const std::unordered_set<const Bus*>*>  RequestHandler::GetBusesByStop(const std::string_view& stop_name) const {
+        const auto& buses_by_stop = db_.GetBusesByStop();
+        auto stop = db_.FindStop(std::string(stop_name));
+        if(!stop) {
+            return std::nullopt;
+        }
+        auto it = buses_by_stop.find(stop);
+        if (it != buses_by_stop.end()) {
+            return &(it->second);
+        }
+        return nullptr;
+    }
 
-bool RequestHandler::HasStop(const std::string_view stop_name) const {
-    return catalogue_.FindStop(stop_name);
+    svg::Document RequestHandler::RenderMap() const {
+        auto info = db_.GetIndexRoutes();
+        std::vector<const Bus*> buses;
+        for(const auto [name, bus_ptr] : info) {
+            buses.push_back(bus_ptr);
+        }
+        return renderer_.MapRender(buses);
+    }
+
 }
