28eb4118dc08fe6feb61817e4c760a405e8b9716
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-19 09:35:17 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,10 +1,18 @@
 #include "json_reader.h"
-#include <iostream>
-using namespace std;
+#include "request_handler.h"
 
 int main() {
-    tc_project::transport_catalogue::TransportCatalogue catalogue;
-    tc_project::map_renderer::MapRenderer renderer;
-    tc_project::json_reader::JsonReader json_reader(catalogue, renderer);
-    json_reader.RequestsProcessing(cin, cout);
+    transport::Catalogue catalogue;
+    JsonReader json_doc(std::cin);
+
+    json_doc.FillCatalogue(catalogue);
+
+    const auto& stat_requests = json_doc.GetStatRequests();
+    const auto& render_settings = json_doc.GetRenderSettings();
+    const auto& renderer = json_doc.FillRenderSettings(render_settings);
+    const auto& routing_settings = json_doc.FillRoutingSettings(json_doc.GetRoutingSettings());
+    const transport::Router router = { routing_settings, catalogue };
+
+    RequestHandler rh(catalogue, renderer, router);
+    json_doc.ProcessRequests(stat_requests, rh);
 }
