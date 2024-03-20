925cf23c8d8c04537bfe8d90e8054093d0bfac74
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-20 09:51:18 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -5,27 +5,50 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <map>
 #include <unordered_map>
 
 namespace transport {
+	namespace type {
+		struct Stop {
+			std::string name;
+			geo::Coordinates coordinates;
+			std::set<std::string> buses_by_stop;
+		};
 
-struct Stop {
-    std::string name;
-    geo::Coordinates coordinates;
-    std::set<std::string> buses_by_stop;
-};
+		struct Bus {
+			std::string number;
+			std::vector<Stop*> stops;
+			bool circle;
+		};
+	}
 
-struct Bus {
-    std::string number;
-    std::vector<const Stop*> stops;
-    bool is_circle;
-};
+	namespace data {
+		struct Stop {
+			std::string name;
+			std::set<std::string> buses_by_stop;
+		};
 
-struct BusStat {
-    size_t stops_count;
-    size_t unique_stops_count;
-    double route_length;
-    double curvature;
-};
+		struct Bus {
+			std::string name;
+			unsigned int stop_count;
+			unsigned int unique_stop_count;
+			double route_length;
+			double curvature;
+		};
+	}
+}
 
-} // namespace transport
+namespace json_reader {
+	struct Stop {
+		std::string stop_name;
+		geo::Coordinates coordinates;
+		std::map<std::string, int> stop_distances;
+	};
+
+	struct Bus {
+		std::string bus_number;
+		std::vector<std::string> stops;
+		bool is_circle;
+	};
+}
