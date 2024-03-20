51d15ecce6699d964df328a6e35503b9d408c000
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-20 09:52:00 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,19 +2,22 @@
 
 #include <cmath>
 
+const int constant = 6371000;
+const double PI = std::atan(1.0) * 4;
+
 namespace geo {
 
-struct Coordinates {
-    double lat;
-    double lng;
-    bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
-    }
-    bool operator!=(const Coordinates& other) const {
-        return !(*this == other);
-    }
-};
+    struct Coordinates {
+        double lat; // Широта
+        double lng; // Долгота
+        bool operator==(const Coordinates& other) const {
+            return lat == other.lat && lng == other.lng;
+        }
+        bool operator!=(const Coordinates& other) const {
+            return !(*this == other);
+        }
+    };
 
-double ComputeDistance(Coordinates from, Coordinates to);
+    double ComputeDistance(Coordinates from, Coordinates to);
 
 }  // namespace geo
