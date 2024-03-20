17053304c17b42cc6bf3beb244e6acf3072f3d2c
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-20 09:46:38 +0300

    Update ranges.h

--- transport-catalogue/ranges.h
+++ transport-catalogue/ranges.h
@@ -6,31 +6,27 @@
 #include <unordered_set>
 
 namespace ranges {
+    template <typename It>
+    class Range {
+    public:
+        using ValueType = typename std::iterator_traits<It>::value_type;
 
-template <typename It>
-class Range {
-public:
-    using ValueType = typename std::iterator_traits<It>::value_type;
+        Range(It begin, It end) : begin_(begin), end_(end) {}
 
-    Range(It begin, It end)
-        : begin_(begin)
-        , end_(end) {
+        It begin() const {
+            return begin_;
+        }
+        It end() const {
+            return end_;
+        }
+
+    private:
+        It begin_;
+        It end_;
+    };
+
+    template <typename C>
+    auto AsRange(const C& container) {
+        return Range{container.begin(), container.end()};
     }
-    It begin() const {
-        return begin_;
-    }
-    It end() const {
-        return end_;
-    }
-
-private:
-    It begin_;
-    It end_;
-};
-
-template <typename C>
-auto AsRange(const C& container) {
-    return Range{ container.begin(), container.end() };
-}
-
 }  // namespace ranges
