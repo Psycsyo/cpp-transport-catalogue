99d5931304adf0caa43cdfdd7db17c3579fded04
Psycsyo <142871245+Psycsyo@users.noreply.github.com>
2024-03-22 09:04:50 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -4,34 +4,37 @@
 #include "transport_catalogue.h"
 
 #include <memory>
+#include <map>
 
 namespace transport {
 
-	class Router {
-	public:
-		Router() = default;
+    struct RouterSettings {
+        int bus_wait_time = 0;
+        double bus_velocity = 0.0;
+    };
 
-		Router(const int bus_wait_time, const double bus_velocity)
-			: bus_wait_time_(bus_wait_time)
-			, bus_velocity_(bus_velocity) {}
+    class Router {
+    public:
+        Router() = default;
 
-		Router(const Router& settings, const Catalogue& catalogue) {
-			bus_wait_time_ = settings.bus_wait_time_;
-			bus_velocity_ = settings.bus_velocity_;
-			BuildGraph(catalogue);
-		}
+        Router(const RouterSettings& settings)
+            : settings_(settings) {}
 
-		const graph::DirectedWeightedGraph<double>& BuildGraph(const Catalogue& catalogue);
-		const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
-		const graph::DirectedWeightedGraph<double>& GetGraph() const;
+        Router(const Router& settings, const Catalogue& catalogue) {
+            settings_ = settings.settings_;
+            BuildGraph(catalogue);
+        }
 
-	private:
-		int bus_wait_time_ = 0;
-		double bus_velocity_ = 0.0;
+        const std::optional<graph::Router<double>::RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
 
-		graph::DirectedWeightedGraph<double> graph_;
-		std::map<std::string, graph::VertexId> stop_ids_;
-		std::unique_ptr<graph::Router<double>> router_;
-	};
+    private:
+        RouterSettings settings_;
+        graph::DirectedWeightedGraph<double> graph_;
+        std::map<std::string, graph::VertexId> stop_ids_;
+        std::unique_ptr<graph::Router<double>> router_;
 
-}
+        void BuildGraph(const Catalogue& catalogue);
+        const graph::DirectedWeightedGraph<double>& GetGraph() const;
+    };
+
+} 
